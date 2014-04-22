9eced35e45a7601ca19c5e1e619afb4161edf42a
Hack Reactor Students <accounts@hackreactor.com>
2014-04-21 23:26:27 -0700

    Beginning to flesh out functions

--- plugins/echo/mousetrap-echo.js
+++ plugins/echo/mousetrap-echo.js
@@ -1,29 +1,46 @@
-(function(Mousetrap) {
-   
-    var _text = [];
+(function (Mousetrap) {
 
-    var _echoCallback;
+  var _record = [];
 
-    var _allowAllKeys;
+  var _callback;
 
-    // store text entered until breaker
-    // reset _handleKey and call callback on text
-    // pass breaker to Mousetrap
+  var _breaker;
 
-    var _handleKey = function(character, modifiers, e) {
-       if (character === breaker) {
-         _echoCallback
-    };
+  var _superHandleKey = Mousetrap.handleKey;
+  // store text entered until breaker
+  // reset _handleKey and call callback on text
+  // pass breaker to Mousetrap
+  //
+  // make for 1 combo then refactor to allow arrays on entry
+
+  var _modifiedCharacter(character, modifiers) {
+
+  }
+
+  var _fireCallback = function (callback) {
+    Mousetrap.handleKey = _superHandleKey;
+    callback(_record);
+  };
+
+  var _handleKey = function(character, modifiers, e) {
+    if (_breaker[character] ||
+      _breaker[_modifiedCharacter(character, modifiers)]) {
+      _fireCallback(_callback);
+      return;
+    }
+
+    _recordKey(character, modifiers);
+  };
 
 
 
 
-    
-    Mousetrap.echo = function(callback, breaker, allowAllCharacters) {
-	_echoCallback = callback;
-	_breaker = breaker;
-	_allowAllKeys = allowAllKeys;
-	Mousetrap.handleKey = _handleKey;
-    };
+
+  Mousetrap.echo = function(callback, breaker) {
+   _callback = callback;
+   _breaker = _parseBreakers(breaker);
+   _allowAllKeys = allowAllKeys;
+   Mousetrap.handleKey = _handleKey;
+  };
 
 })(Mousetrap);
